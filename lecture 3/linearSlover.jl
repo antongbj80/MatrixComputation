10014fb7b1d307049532e152e8805e6a47f0a26a
at2000 <antongbj80@163.com>
2024-04-27 01:08:36 +0800

    2024-4-27

--- lecture 3/linearSlover.jl
+++ lecture 3/linearSlover.jl
@@ -42,47 +42,6 @@ function linear_solver(A,b)
     return back_sub(A,b)
 end
 
-#带排序的高斯消元法（列主元）求解器
-function sort_gauss_elimination(A, b)
-    n = length(b)
-    #排序
-    for k = 1 : n-1  #从1到n-1列，最后一列不需要排序，因为就一个
-        row_max = k #默认第k行k列是最大的
-        for m = k+1 :n  #从k+1到n行
-            #找最大行的位置
-            if abs(A[m,k]) > abs(A[row_max,k])
-                row_max = m
-            end
-        end
-        #判断是否需要交换
-        if row_max != k #需要交换
-            # 使用临时变量temp_row来存储第k行的内容
-            temp_row = A[k, :]
-            temp_b = b[k]
-            
-            # 将第max_row行的内容复制到第k行
-            A[k, :] = A[row_max, :]
-            b[k] = b[row_max]
-            
-            # 将之前第k行（现在存储在temp变量中）的内容复制到第max_row行
-            A[row_max, :] = temp_row
-            b[row_max] = temp_b
-        end
-    end
-    #消元，化上三角
-    for row = 1:n
-        for i = row + 1 : n
-            coef = A[i, row] / A[row, row]
-            for j = row : n
-                A[i, j] = A[i, j] - coef * A[row, j]
-            end
-            b[i] = b[i] - coef * b[row]
-        end
-    end
-    return back_sub(A,b)
-end
-
-
 #LU分解求解器
 function LU_factorization(A,b)
     n = size(A, 1)
@@ -105,10 +64,62 @@ function LU_factorization(A,b)
     return x
 end
 
-#LDL分解求解器
-function LDL(A,b)
-    
+#LDL分解求解器  
+function LDL_factorization(A,b)   
+    n = size(A,1)
+    L = copy(A)
+    D = zeros(n,n)
+    Lt = zeros(n,n)
+    y = copy(b)
+    x = zeros(n)
+
+    #给L和D赋值
+    for i = 1:n
+        for j = i:n
+            if i == j
+                L[i,j] = 1
+                D[i,j] = A[i,i]
+            else
+                L[i,j] = 0
+            end
+        end
+    end
+
+    #求解L和D
+    for i = 1:n
+        for j = 1:i-1
+            for k =1:j-1
+                L[i,j] = L[i,j] - D[k,k]*L[i,k]*L[j,k]
+            end 
+            L[i,j] = L[i,j]/D[j,j]
+        end
+
+        for j = 1:i-1
+            D[i,i] -= D[j,j] * L[i,j]*L[i,j]
+        end 
+    end
+
+    y = forward_sub(L,b)
+
+    #L转置Lt
+    for i = 1:n
+        for j = i:n 
+            if i == j
+                Lt[i,j] = 1
+            else
+                Lt[i,j] = L[j,i]
+            end
+        end
+    end
+    #M = D-1*y
+    M = zeros(n)
+    for i =  1:n
+        M[i] = y[i]/D[i,i]
+    end
+    x = back_sub(Lt,M)
+    return x
 end
+
 
 #三对角矩阵求解（追赶法）器
 function tri_DiagSolver(A,b)
@@ -116,19 +127,18 @@ function tri_DiagSolver(A,b)
     u = zeros(n)
     y = zeros(n)
 
-    u[1] = A[1,2]/A[1,1]
-    y[1] = b[1]/A[1,1]
-    for i = 2:n-1
-        u[i] = A[i,i+1]/(A[i,i]-u[i-1]*A[i,i-1])
-        y[i] = ((b[i]-y[i-1]*A[i,i-1])/(A[i,i]-u[i-1]*A[i,i-1]))        
+    u[1] = A[1,2]/A[1,1]    #1
+    y[1] = b[1]/A[1,1]      #1
+    for i = 2:n-1 
+        u[i] = A[i,i+1]/(A[i,i]-u[i-1]*A[i,i-1])      #n-2
+        y[i] = ((b[i]-y[i-1]*A[i,i-1])/(A[i,i]-u[i-1]*A[i,i-1]))  #n-2        
     end
-    y[n] = ((b[n]-y[n-1]*A[n,n-1])/(A[n,n]-u[n-1]*A[n,n-1]))
+    y[n] = ((b[n]-y[n-1]*A[n,n-1])/(A[n,n]-u[n-1]*A[n,n-1]))       #1
     # 回代求解x
     x = zeros(n)
-    x[n] = y[n]
-    for i = n-1:-1:1
-        x[i] = y[i] - u[i] * x[i+1]
+    x[n] = y[n]    #1
+    for m = n-1:-1:1           
+        x[m] = y[m] - u[m] * x[m+1]          #n-1
     end
     return x
 end
-
